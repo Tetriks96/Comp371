bd8b4716555a15955b6513297393ea1d82eed1dd
David Payette <david.payette@outlook.com>
2019-07-04 20:19:52 -0700

    Adjust camera starting position

--- Framework/Source/World.cpp
+++ Framework/Source/World.cpp
@@ -32,7 +32,7 @@ World::World()
     instance = this;
 
 	// Setup Camera
-	mCamera.push_back(new FirstPersonCamera(vec3(3.0f, 5.0f, 20.0f)));
+	mCamera.push_back(new FirstPersonCamera(vec3(0.0f, 25.0f, 25.0f)));
 	mCamera.push_back(new StaticCamera(vec3(3.0f, 30.0f, 5.0f), vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 1.0f, 0.0f)));
 	mCamera.push_back(new StaticCamera(vec3(0.5f,  0.5f, 5.0f), vec3(0.0f, 0.5f, 0.0f), vec3(0.0f, 1.0f, 0.0f)));
 	mCurrentCamera = 0;
