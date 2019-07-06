85bddbf8cf2b32c602ead3d6e3c5d1c980410998
David Payette <david.payette@outlook.com>
2019-07-05 20:49:21 -0700

    Sphere can change color

--- Framework/Source/SphereModel.h
+++ Framework/Source/SphereModel.h
@@ -14,14 +14,20 @@
 class SphereModel : public Model
 {
 public:
-	SphereModel(glm::vec3 size = glm::vec3(1.0f, 1.0f, 1.0f));
+	SphereModel(glm::vec3 size = glm::vec3(1.0f, 1.0f, 1.0f), glm::vec3 color = glm::vec3(1.0f, 0.05f, 0.05f));
     virtual ~SphereModel(void);
 
     virtual void Update(float dt);
     virtual void Draw();
+
+	virtual void Load(ci_istringstream& iss);
+
+	virtual void Initialize();
     
 protected:
     virtual bool ParseLine(const std::vector<ci_string> &token);
+
+	glm::vec3 mColor;
 
 private:
     // The vertex format could be different for different types of models
