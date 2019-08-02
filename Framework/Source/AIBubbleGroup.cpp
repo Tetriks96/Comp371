1828ffea601103db87365c65746e366c13b8c5d0
checkshumm <checkshumm@gmail.com>
2019-08-02 14:40:17 -0400

    Added pseudo code for AI

--- Framework/Source/AIBubbleGroup.cpp
+++ Framework/Source/AIBubbleGroup.cpp
@@ -1,6 +1,69 @@
 #include "AIBubbleGroup.h"
+#include <map>
+#include <limits>
+using namespace std;
 
 void AIBubbleGroup::Update(float dt)
 {
 
 }
+
+float AIBubbleGroup::getNearestUnitSphere()
+{
+	float minDistance = std::numeric_limits<float>::max();
+	for (int i = 0; i < unitList.size(); i++) {
+		float distance = glm::distance(this.getCenterOfMass() < unitList[i].getCenterOfMass());
+		if ( distance < minDistance) {
+			minDistance = distance;
+		}
+	}
+	return minDistance;
+}
+
+float AIBubbleGroup::getNearestThreat()
+{
+	float minDistance = std::numeric_limits<float>::max();
+	vector<int> threatLevel;
+	vector<int> distances;
+	vector<int> vulnerableBubblesList;
+
+	// loop through all bubble groups
+	for (int i = 0; i < world.getBubbleGroups().size(); i++) {
+		float distance = glm::distance(this.getCenterOfMass() < unitList[i].getCenterOfMass());
+
+		// loop through all bubbles in bubblegroup
+		for (SphereModel& enemyBubble : world.getBubbleGroups()) {
+			// check to see if bubblegroup has a bubble that is larger than any bubble in AI bubble group
+			int vulnerableBubbles = 0;
+			for (SphereModel& aiBubble : this->mSphereModels) {
+				if (aiBubble.radius < enemyBubble.radius) {
+					vulnerableBubbles++;
+					vulnerableBubblesList.push_back(vulnerableBubbles);
+				}
+				else {
+					// fill target list...
+				}
+			}
+			distances.push_back(distance(enemyBubble.centerOfMass, this.getCenterOfMass()))
+		}
+	}
+	return minDistance;
+}
+
+float AIBubbleGroup::getNearestTarget()
+{
+	return 0.0f;
+}
+
+
+
+void AIBubbleGroup::calculateNextPosition()
+{
+	int distanceScore = 0;
+	int targetScore = 0;
+	int vulnerableScore = 0;
+	// loop through vulnerable bubbles list, target list and distances calculate score for each
+	
+
+}
+
--- Framework/Source/AIBubbleGroup.h
+++ Framework/Source/AIBubbleGroup.h
@@ -1,11 +1,26 @@
 #pragma once
 
 #include "BubbleGroup.h"
+#include <map>
 
 class AIBubbleGroup : public BubbleGroup
 {
 public:
 	virtual void Update(float dt);
+
 protected:
+	float getNearestUnitSphere(); // returns the coordinates of the closest unit sphere
+	float getNearestThreat(); // returns the coordinates of the nearest bubble that is larger
+	float getNearestTarget(); // returns the coordinates of the nearest bubble that is smaller
+	void calculateNextPosition(); // find the best position to move to
+	glm::vec3 getDirectionVector(BubbleGroup* enemy);
+	
+
 private:
+	enum Plane { top, bottom, left, right, front, back };
+	std::vector<int> vulnerableBubblesList; // vector of number of bubbles that are vulnerable for each enemy Bubble (should make map?)
+	std::vector<int> threatLevelList; // threat level for each bubble group
+	std::vector<int> targetBubbleList; // target bubble list
+	std::map<BubbleGroup *, Plane> m;
+
 };
